8191f1206bba7c0ce73ee5ada725341f0117aea3
Matthias <m.schuetz@students.htl-leonding.ac.at>
2025-05-17 16:37:42 +0200

    quiz funktionalität

--- project/unterseiten/quiz.js
+++ project/unterseiten/quiz.js
@@ -5,13 +5,30 @@ function showQuiz() {
         <div class="question">
             <div id="questionTitel">${questions[questionNum].titel}</div>
             <div id="answers">
-                <div id="answer">${questions[questionNum].answer1}</div>
-                <div id="answer">${questions[questionNum].answer2}</div>
-                <div id="answer">${questions[questionNum].answer3}</div>
-                <div id="answer">${questions[questionNum].answer4}</div>
+                <div id="answer" onclick="safeAnswer('answer1', ${questionNum})">${questions[questionNum].answer1}</div>
+                <div id="answer" onclick="safeAnswer('answer2', ${questionNum})">${questions[questionNum].answer2}</div>
+                <div id="answer" onclick="safeAnswer('answer3', ${questionNum})">${questions[questionNum].answer3}</div>
+                <div id="answer" onclick="safeAnswer('answer4', ${questionNum})">${questions[questionNum].answer4}</div>
             </div>
         </div><br>`
     }
 }
 
 showQuiz();
+
+let answers = [];
+
+function safeAnswer(answer, questionNumber) {
+    answers[questionNumber] = answer;
+    console.log(answers);
+}
+
+function checkAnswers() {
+    let correctAnswers = 0;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] == questions[i].solution) {
+            correctAnswers++;
+        }
+    }
+    alert("Du hast " + correctAnswers + " von 4" + " Fragen richtig beantwortet.");
+}
