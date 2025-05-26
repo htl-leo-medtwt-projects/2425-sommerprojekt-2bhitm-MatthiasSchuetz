095fa21d823417aad94850f1192a3d0303ef5494
Matthias <m.schuetz@students.htl-leonding.ac.at>
2025-05-26 15:39:05 +0200

    more collectables

--- project/data/collectables.js
+++ project/data/collectables.js
@@ -21,7 +21,7 @@ let collectables = [
         "rarity": "uncommon"
     },
     {
-        "name": "Arc Reaktor",
+        "name": "Arc Reactor",
         "description": "Der Arc Reaktor ist eine starke Energiequelle, die von Tony Stark entwickelt wurde. Er nutzt einen Miniatur-Fusionsreaktor, der saubere, nahezu unbegrenzte Energie liefert und gleichzeitig Starks elektromagnetischen Brustmagneten sowie seinen Iron-Man-Anzug antreibt. Seine enorme Leistungsfähigkeit machen ihn zum Herzstück von Iron Mans Technologie.",
         "img": "../inhalt/Bilder/collect/arc_reactor.png",
         "id": "3",
@@ -35,24 +35,59 @@ let collectables = [
         "rarity": "rare"
     },
     {
-        "name": "Harley's Kartoffelkanone",
+        "name": "Harley's Potatoe Gun",
         "description": "Die Kartoffelkanone ist eine improvisierte Waffe, die von Harley Keener, einem jungen Freund von Tony Stark, gebaut wurde. Sie besteht aus einem einfachen Rohr und verwendet Kartoffeln als Projektile. Obwohl sie nicht sehr mächtig ist, zeigt sie Harleys Einfallsreichtum und seine Fähigkeit, mit begrenzten Ressourcen zu arbeiten.",
         "img": "../inhalt/Bilder/collect/potatoe_gun.png",
         "id": "5",
         "rarity": "epic"
     },
     {
+        "name": "MK5 Suit Case",
+        "description": "Dieser spezielle Koffer enthält den MK5 Ironman Nazug, mit der Tony Stark in Sekundenschnelle einsatzbereit ist. Erstmals gesehen in 'Ironman 2'.",
+        "img": "../inhalt/Bilder/collect/koffer.png",
+        "id": "6",
+        "rarity": "epic"
+    },
+    {
+        "name": "Element 118 – 'Badassium'",
+        "description": "Das fiktive Element, das Tony als neue Energiequelle in 'Iron Man 2' synthetisiert. Extrem mächtig – und komplett einzigartig.",
+        "img": "../inhalt/Bilder/collect/badassium.png",
+        "id": "7",
+        "rarity": "epic"
+    },
+    {
         "name": "Broken Helmet MK50",
         "description": "Tony Stark hat diese Helm von seinem MK50-Anzug verloren, als er in Endgame gegen Thanos gekämpft hat. Der Helm ist stark beschädigt und kann nicht mehr verwendet werden. Er hat ihn verwendet um eine Nachricht an Pepper Potts zu senden, für den Fall dass er nicht zurückkommt.",
         "img": "../inhalt/Bilder/collect/broken_helmet.png",
-        "id": "6",
+        "id": "8",
+        "rarity": "mythic"
+    },
+    {
+        "name": "Proof That Tony Stark Has a Heart",
+        "description": "Dieses ikonische Geschenk von Pepper Potts ist eine Erinnerung an Tonys Wandel. Es handelt sich um seinen ersten Arc-Reaktor, gerahmt in einem Glasbehälter.",
+        "img": "../inhalt/Bilder/collect/proof_tony_has_a_heart.png",
+        "id": "9",
+        "rarity": "mythic"
+    },
+    {
+        "name": "Tony's Hologram to Morgan",
+        "description": "Die letzte Nachricht von Tony Stark an seine Tochter Morgan – aufgenommen vor dem finalen Kampf gegen Thanos. Emotional, bewegend und unvergessen.",
+        "img": "../inhalt/Bilder/collect/hologramm.png",
+        "id": "10",
         "rarity": "mythic"
     },
     {
         "name": "Ironman's Infinity Gauntlet",
         "description": "Der Infinity Gauntlet ist ein mächtiger Handschuh, der von Thanos verwendet wird, um die Infinity-Steine zu kontrollieren. Tony Stark hat eine eigene Version des Infinity Gauntlet entwickelt, um die Macht der Steine zu nutzen und das Universum zu retten. Der Handschuh ist mit fortschrittlicher Technologie ausgestattet und ermöglicht es Stark, die Kräfte der Steine zu kanalisieren.",
         "img": "../inhalt/Bilder/collect/infinity_gauntlet.png",
-        "id": "7",
+        "id": "11",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Morgan's Cheeseburger",
+        "description": "Ein liebevolles Symbol aus 'Endgame': Morgan Stark liebt Cheeseburger – genau wie ihr Vater. Dieses Collectable ist eine Hommage an Tony’s erste Mahlzeit nach seiner Rettung in Iron Man 1.",
+        "img": "../inhalt/Bilder/collect/cheeseburger.png",
+        "id": "12",
         "rarity": "legendary"
     }
 ];
